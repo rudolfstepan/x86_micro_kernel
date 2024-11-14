762cdccb655f89315bf3e345da137b0de3de21ca
Rudolf Stepan <rudolf.stepan@outlook.at>
2024-11-14 15:23:22 +0100

    ...

--- src/boot/irq.asm
+++ src/boot/irq.asm
@@ -1,22 +1,5 @@
 [BITS 32]
-
-global irq0
-global irq1
-global irq2
-global irq3
-global irq4
-global irq5
-global irq6
-global irq7
-global irq8
-global irq9
-global irq10
-global irq11
-global irq12
-global irq13
-global irq14
-global irq15
-
+global irq0, irq1, irq2, irq3, irq4, irq5, irq6, irq7, irq8, irq9, irq10, irq11, irq12, irq13, irq14, irq15
 extern irq_handler
 
 section .text
