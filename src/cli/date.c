ce57186b6b60ae0626427142a261d4f918704739
Rudolf Stepan <rudolf.stepan@outlook.at>
2024-12-01 19:00:56 +0100

    fixed flickering

--- src/cli/date.c
+++ src/cli/date.c
@@ -11,28 +11,39 @@ int main() {
 
     // Store the initial cursor position
     int initial_x, initial_y;
-    
+
+    // Variables to store previous and current time
+    int prev_hour = -1, prev_minute = -1, prev_second = -1;
+    int year, month, day, hour, minute, second;
 
     while (1) {
         get_cursor_position(&initial_x, &initial_y);
-        // Read date and time
-        int year, month, day, hour, minute, second;
+
+        // Read the current date and time
         read_date(&year, &month, &day);
         read_time(&hour, &minute, &second);
 
-        // Format the time as a string
-        char time_str[9]; // HH:MM:SS\0
-        snprintf(time_str, sizeof(time_str), "%d:%d:%d", hour, minute, second);
+        // Check if the time has changed
+        if (hour != prev_hour || minute != prev_minute || second != prev_second) {
+            // Update the previous time
+            prev_hour = hour;
+            prev_minute = minute;
+            prev_second = second;
 
-        // Display the time in the top-right corner
-        set_cursor_position(TIME_COL, TIME_ROW);
-        printf("%s", time_str);
+            // Format the time as a string
+            char time_str[9]; // HH:MM:SS\0
+            snprintf(time_str, sizeof(time_str), "%d:%d:%d", hour, minute, second);
 
-        // Restore the original cursor position
-        set_cursor_position(initial_x, initial_y);
+            // Display the time in the top-right corner
+            set_cursor_position(TIME_COL, TIME_ROW);
+            printf("%s", time_str);
 
-        // Small delay to reduce flicker
-        asm volatile("int $0x29"); // Trigger a timer interrupt
+            // Restore the original cursor position
+            set_cursor_position(initial_x, initial_y);
+        }
+
+        // Trigger a timer interrupt (non-blocking, allows context switching)
+        asm volatile("int $0x29");
     }
 
     return 0;
